7084461d86d546f0c5f6dd54bd63b95b8020b41d
joaovictor1020 <39501506+joaovictor1020@users.noreply.github.com>
2018-06-18 21:40:24 -0300

    Update palindrome.c

--- Q2/palindrome.c
+++ Q2/palindrome.c
@@ -14,9 +14,28 @@
 bool isPalindrome (char sentence[], int length) {
     printf("\nisPalindrome::START: %s, %d\n", sentence, length);
 
-    // TODO: YOUR CODE HERE!
+    bool temp = true;
+     if (length % 2 != 0) {
+         int half = (length -1) / 2;
+         for (int i = 0; i < half; ++i) {
+             if (sentence[i] != sentence[length - 1 - i]) {
+                 temp = false;
+             }
+         }
+     } else {
+         temp = false;
+     }
+     if (temp) {
+         printf("true\n");
+     } else {
+         printf("false\n");
+     }
+   
+    
     
     printf("\nisPalindrome::END\n");
+     if (temp) {
+         return true;
     return false;
 }
 
